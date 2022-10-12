ee00861a5398a82cbc169a336c697bdcb10bc6cc
aurorg <2519652258@qq.com>
2022-10-12 21:25:36 +0800

    1

--- GolandProjects/go_learn/test_one/chapter06/demo10/main.go
+++ GolandProjects/go_learn/test_one/chapter06/demo10/main.go
@@ -16,6 +16,8 @@ func AddUpper() func(int) int {
 		return n
 	}
 }
+
+// 返回的匿名函数和makeSuffix(suffix string)的suffix变量和返回的函数组合成一个闭包，因为返回的函数引用到suffix这个变量
 func makeSuffix(suffix string) func(string) string {
 	return func(name string) string {
 		if !strings.HasSuffix(name, suffix) {
@@ -24,13 +26,25 @@ func makeSuffix(suffix string) func(string) string {
 		return name
 	}
 }
+
+// 不使用闭包，普通方法
+func makeSuffix2(suffix string, name string) string {
+	if !strings.HasSuffix(name, suffix) {
+		return name + suffix
+	}
+	return name
+}
 func main() {
 	f := AddUpper()
 	fmt.Println(f(1)) //11
 	fmt.Println(f(2)) //13闭包
 	fmt.Println(f(3)) //16
 
-	f1 := makeSuffix(".jpg")
+	f1 := makeSuffix(".jpg") //使用闭包只用传一次后缀
 	fmt.Println("文件处理后=", f1("winter"))
 	fmt.Println("文件处理后=", f1("hahaha.jpg"))
+
+	//普通方法每一次都要传入后缀
+	fmt.Println("文件处理后=", makeSuffix2("jpg", "winter"))
+	fmt.Println("文件处理后=", makeSuffix2("jpg", "hahaha.jpg"))
 }
