1365645ccd6e4a02402ac4c046a8b5de53855579
aurorg <2519652258@qq.com>
2022-10-13 10:26:38 +0800

    1

--- GolandProjects/go_learn/test_one/chapter06/deferdome/main.go
+++ GolandProjects/go_learn/test_one/chapter06/deferdome/main.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func sum(n1 int, n2 int) int {
+	//当执行到defer时，会将后面的语句压入到独立的栈中（defer栈）
+	//当函数执行完毕后，再从defer栈中，按照先入后出的方式出栈
 	defer fmt.Println("ok1 n1=", n1)
 	defer fmt.Println("ok2 n2=", n2)
+
 	res := n1 + n2
 	fmt.Println("ok3 res=", res)
 	return res
 }
 func main() {
-	sum(10, 20)
+	res := sum(10, 20)
+	fmt.Println("res=", res)
 }
