e5235b569834acfac50de0c2e359bc7d5b7a4b7d
aurorg <2519652258@qq.com>
2022-10-15 15:56:59 +0800

    1

--- GolandProjects/go_learn/test_one/chapter07/arraydetails/main.go
+++ GolandProjects/go_learn/test_one/chapter07/arraydetails/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+func test01(arr [3]int) {
+	arr[0] = 88
+}
+
+func test02(arr *[3]int) {
+	(*arr)[0] = 88
+}
+
 func main() {
-	
+	//arr := [3]int{11, 22, 33}
+	//test01(arr)
+	//fmt.Println(arr)
+	arr := [3]int{11, 22, 33}
+	test02(&arr)
+	fmt.Println(arr)
 }
