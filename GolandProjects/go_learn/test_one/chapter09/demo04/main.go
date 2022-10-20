d1837809f18ecd60e25aed44e5e658a73f728f97
aurorg <2519652258@qq.com>
2022-10-20 21:00:21 +0800

    1

---
+++ GolandProjects/go_learn/test_one/chapter09/demo04/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	map1 := make(map[int]int, 10)
+	map1[10] = 100
+	map1[1] = 11
+	map1[4] = 33
+	map1[8] = 90
+	fmt.Println(map1)
+
+	var keys []int
+	for k, _ := range map1 {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	fmt.Println(keys)
+
+	for _, k := range keys {
+		fmt.Printf("map1[%v]=%v \n", k, map1[k])
+	}
+}
